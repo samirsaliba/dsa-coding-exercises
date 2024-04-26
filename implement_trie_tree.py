f09e4d3ba156cf7cba636864c6b644d0c86a6227
Samir Saliba <samirtsj@gmail.com>
2024-04-26 20:01:23 -0300

    Add description to implement tree exercise

--- implement_trie_tree.py
+++ implement_trie_tree.py
@@ -1,6 +1,25 @@
+# 208. Implement Trie (Prefix Tree)
+
+# A trie (pronounced as "try") or prefix tree is a tree data structure 
+# used to efficiently store and retrieve keys in a dataset of strings. 
+# There are various applications of this data structure, 
+# such as autocomplete and spellchecker.
+
+# Implement the Trie class:
+
+#Trie() Initializes the trie object.
+# void insert(String word) Inserts the string word into the trie.
+# boolean search(String word) Returns true if the string word is in the trie
+# (i.e., was inserted before), and false otherwise.
+
+# boolean startsWith(String prefix) Returns true if 
+# there is a previously inserted string word that has the prefix prefix, 
+# and false otherwise.
+
+
 class TreeNode:
     def __init__(self):
-        self.children = [None] * 26 # number of letters in the english alphabet
+        self.children = [None] * 26 # english alphabet
         self.end = False
 
 class Trie:
